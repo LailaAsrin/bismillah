41170d12bb403748217af3c002eb1a413d637b90
aliencyy <kelsyamirah62@gmail.com>
2023-05-02 14:37:03 +0700

    docs: modify some file

--- resources/views/auth/LoginPage.blade.php
+++ resources/views/auth/LoginPage.blade.php
@@ -20,8 +20,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
-    <title>Soft UI Dashboard Tailwind</title>
+    <!-- Favicon icon -->
+    <link rel="icon" href="https://simkuliah.usk.ac.id/assets/unsyiah.png" type="image/x-icon">
+    <title>SIMBA</title>
     <!-- Fonts and icons -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Font Awesome Icons -->
@@ -54,8 +55,8 @@
                                 <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
                                     <h3
                                         class="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">
-                                        Welcome back</h3>
-                                    <p class="mb-0">Enter your email and password to sign in</p>
+                                        Selamat Datang!</h3>
+                                    <p class="mb-0">Masukkan email dan password Anda untuk masuk</p>
                                 </div>
                                 <div class="flex-auto p-6">
                                     <form role="form">
@@ -78,22 +79,20 @@
                                                 type="checkbox" checked="" />
                                             <label
                                                 class="mb-2 ml-1 font-normal cursor-pointer select-none text-sm text-slate-700"
-                                                for="rememberMe">Remember me</label>
+                                                for="rememberMe">Ingat saya</label>
                                         </div>
                                         <div class="text-center">
                                             <button type="button"
-                                                class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-blue-600 to-cyan-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">Sign
-                                                in</button>
+                                                class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-blue-600 to-cyan-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">Masuk</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div
                                     class="p-6 px-1 pt-0 text-center bg-transparent border-t-0 border-t-solid rounded-b-2xl lg:px-2">
                                     <p class="mx-auto mb-6 leading-normal text-sm">
-                                        Don't have an account?
+                                        Belum punya akun?
                                         <a href="../pages/sign-up.html"
-                                            class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Sign
-                                            up</a>
+                                            class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Daftar</a>
                                     </p>
                                 </div>
                             </div>
