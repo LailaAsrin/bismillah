52c6ae82be567660359d1af845cf3945f923d87a
aliencyy <kelsyamirah62@gmail.com>
2023-05-03 13:27:18 +0800

    docs: modify dataalumni.blade.php

--- resources/views/dataalumni.blade.php
+++ resources/views/dataalumni.blade.php
@@ -372,8 +372,7 @@
                                                 </div>
                                             </td>
                                             <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                                <p class="mb-0 font-semibold leading-tight text-xs">Programator</p>
-                                                <p class="mb-0 leading-tight text-xs text-slate-400">Developer</p>
+                                            <p class="mb-0 font-semibold leading-tight text-xs">2078906544878</p>
                                             </td>
                                             <td class="p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent">
                                                 <span class="bg-gradient-to-tl from-slate-600 to-slate-300 px-3.6 text-xs rounded-1.8 py-2.2 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white">Offline</span>
